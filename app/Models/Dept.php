b9589a423524f7fa6ac364af9a3cc882e01c83d7
sighaloic <sighaloic@gmail.com>
2024-10-12 13:24:53 +0200

    updating debt controller, adding implementation

--- app/Models/Dept.php
+++ app/Models/Dept.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Dept extends Model
 {
     use HasFactory;
+    protected $table = 'debts';
+    protected $fillable = ['cashflow_id', 'pay_year', 'ref_ing_cost', 'member_id', 'amount', 'pay', 'status', 'personnel_id', 'author'];
 }
