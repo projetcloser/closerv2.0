68d9deba1f4934796b76d6a6cd3ac827eefecc65
sighaloic <sighaloic@gmail.com>
2024-10-12 13:12:58 +0200

    feat/debts CRUD

---
+++ app/Http/Controllers/API/DeptsApiController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Dept;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeptsApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dept $dept)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dept $dept)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dept $dept)
+    {
+        //
+    }
+}
