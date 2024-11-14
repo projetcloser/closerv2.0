2059cf50a20166587f3ed1d2d1a7ca3ec5c88f99
sighaloic <sighaloic@gmail.com>
2024-11-14 17:32:02 +0100

    (feat)[test] payment API

---
+++ app/Models/PaymentPayload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentPayload extends Model
+{
+    use HasFactory;
+    protected $table = 'payment_payloads';
+    protected $fillable = ['form_data', 'request_result'];
+}
---
+++ database/migrations/2024_11_13_224235_create_payment_payloads_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_payloads', function (Blueprint $table) {
+            $table->id();
+            $table->longText('request_result');
+            $table->longText('form_data');
+            $table->boolean('open_close')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_payloads');
+    }
+};
